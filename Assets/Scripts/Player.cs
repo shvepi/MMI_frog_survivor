65acbbccc417e10ea17ea26229ed5a7cc112012d
Iyed-Gh <133121177+Iyed-Gh@users.noreply.github.com>
2023-07-09 18:51:43 +0200

    Background + Voice commands

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Windows.Speech;
 
 public class Player : MonoBehaviour
 {
@@ -13,33 +16,67 @@ public class Player : MonoBehaviour
     [SerializeField]
     private Animator anim;
 
-    private int _direction = 1;
+    private int _direction = 0;
 
     public int Direction { get { return _direction; } }
+    private Dictionary<string, Action> keywordActions = new Dictionary<string, Action>();
+    private KeywordRecognizer keywordRecognizer;
 
     private void Awake()
     {
         if (instance == null)
             instance = this;
+        keywordActions.Add("stop", Stop);
+        keywordActions.Add("left", Left);
+        keywordActions.Add("right", Right);
+
+        keywordRecognizer = new KeywordRecognizer(keywordActions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += OnKeywordRecognized;
+        keywordRecognizer.Start();
+        
     }
 
+    private void OnKeywordRecognized (PhraseRecognizedEventArgs args)
+    {
+        Debug.Log("Keyword: " + args.text);
+        keywordActions[args.text].Invoke();
+    }
+
+    private void Stop()
+    {
+        _direction = 0;
+    }
+
+    private void Left()
+    {
+        _direction = -1;
+    }
+
+    private void Right()
+    {
+        _direction = 1;
+    }
     // Update is called once per frame
     void Update()
     {
+        // Keyboard input.
         float horizontalInput = Input.GetAxis("Horizontal");
-        _direction = (horizontalInput != 0) ? (int)Mathf.Sign(horizontalInput) : 0;
-        transform.localScale = new Vector3(_direction != 0 ? _direction : transform.localScale.x, 0.5f, 1);
+        int directionKeyboard = (horizontalInput != 0) ? (int)Mathf.Sign(horizontalInput) : 0;
 
-        if (_direction != 0)
+        // Combine keyboard and voice input. You might need to handle the case when both inputs are active.
+        int combinedDirection = _direction != 0 ? _direction : directionKeyboard;
+
+        transform.localScale = new Vector3(combinedDirection != 0 ? combinedDirection : transform.localScale.x, 0.5f, 1);
+
+        if (combinedDirection != 0)
         {
-            transform.position += Vector3.right * moveSpeed * Time.deltaTime * _direction;
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime * combinedDirection;
             transform.position = new Vector3(Mathf.Clamp(transform.position.x, -xLimit, xLimit), transform.position.y, transform.position.z);
         }
 
-        if (_direction != 0)
+        if (combinedDirection != 0)
         {
             anim.SetBool("Start", true);
-
         }
         else
         {
